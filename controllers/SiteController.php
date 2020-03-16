e9306be63c6de338aba5953b03b3b52d35a77d10
serj <vitte@bk.ru>
2020-03-16 14:37:10 +0300

    Черновой вариант авторизации создания и обновления пользователей

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,128 +1,43 @@
 <?php
-
 namespace app\controllers;
 
 use Yii;
+use yii\rest\Controller;
+use app\models\LoginForm;
 use yii\filters\AccessControl;
-use yii\web\Controller;
 use yii\web\Response;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
 use app\models\ContactForm;
+use yii\rest\ActiveController;
 
-class SiteController extends Controller
+class SiteController  extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
-
-    /**
-     * Displays homepage.
-     *
-     * @return string
-     */
+    public $modelClass = 'app\models\User';
     public function actionIndex()
     {
-        return $this->render('index');
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+         return $this->asJson('Тест');
+  
     }
 
-    /**
-     * Login action.
-     *
-     * @return Response|string
-     */
+
+
     public function actionLogin()
     {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
         $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
+        $model->load(Yii::$app->request->bodyParams, '');
+        if ($token = $model->auth()) {
+            return $token;
+        } else {
+            return $model;
         }
-
-        $model->password = '';
-        return $this->render('login', [
-            'model' => $model,
-        ]);
     }
 
-    /**
-     * Logout action.
-     *
-     * @return Response
-     */
-    public function actionLogout()
+    protected function verbs()
     {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
-    }
-
-    /**
-     * Displays contact page.
-     *
-     * @return Response|string
-     */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Displays about page.
-     *
-     * @return string
-     */
-    public function actionAbout()
-    {
-        return $this->render('about');
+        return [
+            'login' => ['post'],
+             'index' => ['POST']
+        ];
     }
 }
--- models/User.php
+++ models/User.php
@@ -1,104 +1,171 @@
 <?php
-
 namespace app\models;
+use yii\base\NotSupportedException;
+use Yii;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+use yii\db\Expression;
+use yii\behaviors\TimestampBehavior;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\auth\QueryParamAuth;
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $auth_key
+ * @property string $password_hash
+ * @property string $password_reset_token
+ * @property string $email
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property Calendar[] $calendars
+ */
+class User extends ActiveRecord implements IdentityInterface
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
 
     /**
      * {@inheritdoc}
      */
+    public function rules()
+    {
+        return [
+            [['name', 'auth_key', 'hash_pass', 'email'], 'required'],
+          
+            [['name', 'hash_pass', 'passwd_reset_token', 'email'], 'string'],
+         
+            [['name'], 'unique'],
+            [['email'], 'unique'],
+            [['passwd_reset_token'], 'unique'],
+            [['email'],'email']
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+          
+            'name' => 'Имя пользователя',
+            
+            'hash_pass' => 'Пароль',
+           
+            'email' => 'Email',
+           
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+
+    public function fields(){
+        return ['id','email','name'];
+    }
+
+
     public static function findIdentity($id)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+             return static::findOne(['id' => $id]);
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
+        /**
+        * {@inheritdoc}
+        */
+     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return  static::findOne(['auth_key' => $token]);
     }
-
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
+        /**
+        * Finds user by username
+        *
+        * @param string $username
+        * @return static|null
+        */
+    public static function findByUsername($name)
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return static::findOne(['name' => $name]);
     }
-
-    /**
-     * {@inheritdoc}
-     */
+        /**
+        * {@inheritdoc}
+        */
     public function getId()
     {
-        return $this->id;
+        return $this->getPrimaryKey();
     }
+        /**
+        * {@inheritdoc}
+        */
 
-    /**
-     * {@inheritdoc}
-     */
     public function getAuthKey()
     {
-        return $this->authKey;
+        return $this->auth_key;
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function validateAuthKey($authKey)
+        /**
+        * {@inheritdoc}
+        */
+     public function validateAuthKey($authKey)
     {
-        return $this->authKey === $authKey;
+        return $this->getAuthKey() === $authKey;
     }
-
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
+        /**
+        * Validates password
+        *
+        * @param string $password password to validate
+        * @return bool if password provided is valid for current user
+        */
     public function validatePassword($password)
     {
-        return $this->password === $password;
+        return  Yii::$app->security->validatePassword($password,$this->hash_pass);
     }
+        /**
+        * Generates password hash from password and sets it to the model
+        *
+        * @param string $password
+        */
+    public function setPassword($password)
+    {
+        $this->hash_pass = Yii::$app->security->generatePasswordHash($password);
+    }
+        /**
+        * Generates "remember me" authentication key
+        */
+
+    public function generateAuthKey()
+    {
+        $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+    public function getActivity()
+    {
+    
+    return $this->hasOne(Activity::className(),['id_user' => 'id']);
+    }
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                HttpBasicAuth::className(),
+                HttpBearerAuth::className(),
+                QueryParamAuth::className(),
+            ],
+        ];
+        return $behaviors;
+     }
+
 }
